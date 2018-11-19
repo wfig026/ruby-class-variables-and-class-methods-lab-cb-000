f2231fdc902b5983fb4e2f753c10e892d5a83454
Will Figueiredo <will@wfigueiredo.com>
2018-11-19 17:52:31 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,37 @@
+class Song
+  
+  attr_accessor :name, :artist, :genre
+  @@count = 0
+  @@artists = []
+  @@genres = []
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @@artists << artist
+    @genre = genre
+    @@genres << genre
+    @@count += 1
+  end
+
+  def self.count
+    return @@count
+  end
+
+  def self.artists
+    return @@artists.unique
+  end
+
+  def self.genres
+    return @@genres.unique
+  end
+
+  def self.genre_count
+    @@genres.uniq.map{|genre| [genre, @@genres.count(genre)]}.to_h
+  end
+  
+  def self.artist_count
+    @@artists.uniq.map{|artist| [artist, @@artists.count(artist)]}.to_h
+  end
+  
+end
